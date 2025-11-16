0b6f8efa9e079fa152db03ceee88b2ebdbdd4b44
abc202306 <135524584+abc202306@users.noreply.github.com>
2025-11-16 13:03:29 +0800

    fix: refactored the code

--- src/exhentai-web-clipper-for-obsidian.js
+++ src/exhentai-web-clipper-for-obsidian.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EXHentai Web Clipper for Obsidian
 // @namespace    https://exhentai.org
-// @version      v1.0.6.20251115
+// @version      v1.0.7.20251116
 // @description  🔞 A user script that exports EXHentai gallery metadata as Obsidian Markdown files (Obsidian EXHentai Web Clipper).
 // @author       abc202306
 // @match        https://exhentai.org/g/*
@@ -13,185 +13,160 @@
 (function () {
   'use strict';
 
-  // Entry point
-  setTimeout(startEXHentaiWebclipper, 2000);
-
-  async function startEXHentaiWebclipper() {
-    const data = getData();
-    const fileContent = await getFileContent(data);
-    const obsidianURI = getObsidianURI(data.title, fileContent);
-
-    if (confirm("EXHentai Web Clipper for Obsidian (a tampermonkey user script by abc202306) says:\n\nDo you want to proceed to clip the exhentai gallery metadata as a obsidian markdown note (by obsidian uri protocol api)?\n\nclick 'OK' to proceed, or 'Cancel' to abort.")) {
-      window.location.href = obsidianURI;
+  class Main {
+    util;
+    
+    // Entry point
+    static main(){
+      new Main(new Util());
     }
-  }
 
-  // Build Obsidian URI
-  function getObsidianURI(title, fileContent) {
-    const params = [
-      ["file", `acg/galleries/${title}`],
-      ["content", fileContent],
-      ["append", "1"]
-    ].map(([k, v]) => `${k}=${encodeURIComponent(v)}`).join("&");
+    constructor(util) {
+      this.util = util;
+      this.util.startWebclipperWithDelay(
+        2000,
+        "EXHentai Web Clipper for Obsidian (a tampermonkey user script by abc202306) says:\n\nDo you want to proceed to clip the exhentai gallery metadata as a obsidian markdown note (by obsidian uri protocol api)?\n\nclick 'OK' to proceed, or 'Cancel' to abort.",
+        this.getEXHentaiGalleryData.bind(this),
+        this.getEXHentaiOBMDNoteFileContent.bind(this)
+      );
+    }
 
-    return `obsidian://new?${params};`;
-  }
+    // Extract metadata from page
+    getEXHentaiGalleryData() {
+      const titleEN = this.util.getTitleStr(document.getElementById("gn"));
+      const titleJP = this.util.getTitleStr(document.getElementById("gj"));
 
-  // Extract metadata from page
-  function getData() {
-    const titleEN = getTitleStr(document.getElementById("gn"));
-    const titleJP = getTitleStr(document.getElementById("gj"));
+      const now = this.util.getLocalISOStringWithTimezone();
 
-    const now = getLocalISOStringWithTimezone();
-
-    const data0 = Object.fromEntries([...document.getElementById("gdd").firstChild.firstChild.childNodes].map(c => {
-      let key = c.children[0].innerText.replace(/:$/, "").toLowerCase().replaceAll(/\s/g, "");
-      let value;
-      if (key === "posted") {
-        key = "uploaded";
-        const postedTimeData = /^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2}) (?<hour>\d{2}):(?<minute>\d{2})$/.exec(c.children[1].innerText).groups;
-        value = postedTimeData.year + "-" + postedTimeData.month + "-" + postedTimeData.day + "T" + postedTimeData.hour + ":" + postedTimeData.minute + ":00Z";
-      } else if (key === "parent") {
-        value = c.children[1].firstChild.href || c.children[1].innerText;
-      } else if (key === "visible") {
-        value = c.children[1].innerText;
-      } else if (key === "language") {
-        value = c.children[1].innerText.split(/\s+/).map(i => (i === "TR") ? ("[[translated]]") : ("[[" + i + "]]"));
-      } else if (key === "filesize") {
-        value = c.children[1].innerText;
-      } else if (key === "length") {
-        key = "pagecount";
-        value = parseInt(c.children[1].innerText.replace(/ pages$/, ""));
-      } else if (key === "favorited") {
-        value = parseInt(c.children[1].innerText.replace(/ times$/, ""));
-      } else {
-        value = c.children[1].innerText;
-      }
-      return [key, value];
-    }));
+      const data0 = Object.fromEntries([...document.getElementById("gdd").firstChild.firstChild.childNodes].map(c => {
+        let key = c.children[0].innerText.replace(/:$/, "").toLowerCase().replaceAll(/\s/g, "");
+        let value;
+        if (key === "posted") {
+          key = "uploaded";
+          const postedTimeData = /^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2}) (?<hour>\d{2}):(?<minute>\d{2})$/.exec(c.children[1].innerText).groups;
+          value = postedTimeData.year + "-" + postedTimeData.month + "-" + postedTimeData.day + "T" + postedTimeData.hour + ":" + postedTimeData.minute + ":00Z";
+        } else if (key === "parent") {
+          value = c.children[1].firstChild.href || c.children[1].innerText;
+        } else if (key === "visible") {
+          value = c.children[1].innerText;
+        } else if (key === "language") {
+          value = c.children[1].innerText.split(/\s+/).map(i => (i === "TR") ? ("[[translated]]") : ("[[" + i + "]]"));
+        } else if (key === "filesize") {
+          value = c.children[1].innerText;
+        } else if (key === "length") {
+          key = "pagecount";
+          value = parseInt(c.children[1].innerText.replace(/ pages$/, ""));
+        } else if (key === "favorited") {
+          value = parseInt(c.children[1].innerText.replace(/ times$/, ""));
+        } else {
+          value = c.children[1].innerText;
+        }
+        return [key, value];
+      }));
 
 
-    const gidPairResult = /^https?:\/\/e[x\-]hentai.org\/g\/(\d*)\/([a-z\d]*)\/?/.exec(window.location.href);
-    const galleryID = gidPairResult ? gidPairResult[1] : null;
-    const galleryToken = gidPairResult ? gidPairResult[2] : null;
+      const gidPairResult = /^https?:\/\/e[x\-]hentai.org\/g\/(\d*)\/([a-z\d]*)\/?/.exec(window.location.href);
+      const galleryID = gidPairResult ? gidPairResult[1] : null;
+      const galleryToken = gidPairResult ? gidPairResult[2] : null;
 
-    const data = {
-      title: sanitizeTitle(titleJP || titleEN),
-      english: titleEN,
-      japanese: titleJP,
-      url: window.location.href,
+      const data = {
+        title: this.util.sanitizeTitle(titleJP || titleEN, " 【exhentai】"),
+        english: titleEN,
+        japanese: titleJP,
+        url: window.location.href,
 
-      coverPromise: fetch('https://api.e-hentai.org/api.php', { method: "POST", body: JSON.stringify({ "method": "gdata", "gidlist": [[galleryID, galleryToken]], "namespace": 1 }) }).then(response => { console.log(response); return response.json(); }).then(json => { console.log(json); return json.gmetadata[0].thumb; }),
+        coverPromise: fetch('https://api.e-hentai.org/api.php', { method: "POST", body: JSON.stringify({ "method": "gdata", "gidlist": [[galleryID, galleryToken]], "namespace": 1 }) }).then(response => response.json()).then(json => json.gmetadata[0].thumb),
 
-      categories: ["[[" + document.getElementById("gdc").innerText + "]]"], // gd3.Category => categories
+        categories: ["[[" + document.getElementById("gdc").innerText + "]]"], // gd3.Category => categories
 
-      uploader: ["[[" + document.getElementById("gdn").innerText + "]]"], // gd3.Uploader => uploader
+        uploader: ["[[" + document.getElementById("gdn").innerText + "]]"], // gd3.Uploader => uploader
 
-      uploaded: data0.uploaded, // gd3.Posted => uploaded
-      parent: data0.parent, // gd3.Parent => parent
-      visible: data0.visible, // gd3.Visible => visible
-      language: data0.language, // gd3.Language and gd4.language => language
-      filesize: data0.filesize, // gd3.Filesize => filesize
-      pagecount: data0.pagecount, // gd3.length => pagecount
-      favorited: data0.favorited, // gd3.Favorited => favorited
+        uploaded: data0.uploaded, // gd3.Posted => uploaded
+        parent: data0.parent, // gd3.Parent => parent
+        visible: data0.visible, // gd3.Visible => visible
+        language: data0.language, // gd3.Language and gd4.language => language
+        filesize: data0.filesize, // gd3.Filesize => filesize
+        pagecount: data0.pagecount, // gd3.length => pagecount
+        favorited: data0.favorited, // gd3.Favorited => favorited
 
-      rating: parseFloat(document.getElementById("rating_label").innerText.replace(/Average: ([\d\.]*)/, "$1")),
+        rating: parseFloat(document.getElementById("rating_label").innerText.replace(/Average: ([\d\.]*)/, "$1")),
 
-      ctime: now,
-      mtime: now,
+        ctime: now,
+        mtime: now,
 
-      keywords: [],
-      parody: [],
-      character: [],
-      artist: [],
-      group: [],
-      language: [],
-      female: [],
-      male: [],
-      mixed: [],
-      location: [],
-      other: [],
-      unindexedData: {}
-    };
+        keywords: [],
+        parody: [],
+        character: [],
+        artist: [],
+        group: [],
+        language: [],
+        female: [],
+        male: [],
+        mixed: [],
+        location: [],
+        other: [],
+        unindexedData: {}
+      };
 
-    [...document.getElementById("taglist").firstChild.firstChild.children].map(c => {
-      const key = c.children[0].innerText.replace(/:$/, "").toLowerCase().replaceAll(/\s/g, "");
-      const value = c.children[1].innerText.split("\n").map(i => "[[" + getTagNameStr(i) + "]]");
+      const dataKeyIndexed = [
+        "title", "english", "japanese", "url", "coverPromise",
+        "categories",
+        "uploader", "uploaded",
+        "parent", "visible", "language", "filesize", "pagecount", "favorited", "rating",
+        "ctime", "mtime",
+        "keywords",
+        "parody", "character", "artist", "group", "language", "female", "male", "mixed", "location", "other",
+        "unindexedData"
+      ];
 
-      if (Array.isArray(data[key])) {
-        data[key] = data[key].concat(value);
-      } else if (data[key]) {
-        data[key] = [data[key]].concat(value);
-      } else {
-        data[key] = value;
-      }
-    });
+      [...document.getElementById("taglist").firstChild.firstChild.children].map(c => {
+        const key = c.children[0].innerText.replace(/:$/, "").toLowerCase().replaceAll(/\s/g, "");
+        const value = c.children[1].innerText.split("\n").map(i => "[[" + this.util.getTagNameStr(i) + "]]");
 
-    const dataKeyIndexed = [
-      "title", "english", "japanese", "url", "coverPromise",
-      "categories",
-      "uploader", "uploaded",
-      "parent", "visible", "language", "filesize", "pagecount", "favorited", "rating",
-      "ctime", "mtime",
-      "keywords",
-      "parody", "character", "artist", "group", "language", "female", "male", "mixed", "location", "other",
-      "unindexedData"
-    ];
+        let newValue;
 
-    data.unindexedData = Object.fromEntries(Object.keys(data).filter(key => !dataKeyIndexed.includes(key)).map(key => [key, data[key]]));
+        if (Array.isArray(data[key])) {
+          newValue = data[key].concat(value);
+        } else if (data[key]) {
+          newValue = [data[key]].concat(value);
+        } else {
+          newValue = value;
+        }
 
-    return data;
-  } 
+        if (dataKeyIndexed.includes(key)) {
+          data[key] = newValue;
+        } else {
+          data.unindexedData[key] = newValue;
+        }
+        data[key] = newValue;
+      });
 
-  function getUnindexedDataFrontMatterPartStrBlock(unindexedData) {
-    let unindexedDataFrontMatterPartStrBlock = '';
-    Object.entries(unindexedData).forEach(([key,value]) => {
-      if (Array.isArray(value)) {
-        unindexedDataFrontMatterPartStrBlock += `\n${key}:${getYamlArrayStr(value)}`;
-      } else {
-        unindexedDataFrontMatterPartStrBlock += `\n${key}: "${value}"`;
-      }
-    });
-    return unindexedDataFrontMatterPartStrBlock;
-  }
+      return data;
+    }
 
-  function getUnindexedDataTablePartStrBlock(unindexedData) {
-    let unindexedDataTablePartStrBlock = '';
-    Object.entries(unindexedData).forEach(([key,value]) => {
-      if (Array.isArray(value)) {
-        unindexedDataTablePartStrBlock += `\n| ${key} | ${value.join(", ")} |`;
-      } else {
-        unindexedDataTablePartStrBlock += `\n| ${key} | ${value} |`;
-      }
-    });
-    return unindexedDataTablePartStrBlock;
-  }
-
-  // Build Obsidian note content
-  async function getFileContent(data) {
-    const escapePipe = str => str.replace(/\|/g, "\\|");
-
-    return `---
+    // Build Obsidian note content
+    async getEXHentaiOBMDNoteFileContent(data) {
+      return `---
 up:
   - "[[Gallery]]"
-categories:${getYamlArrayStr(data.categories)}
-keywords:${getYamlArrayStr(data.keywords)}
-female:${getYamlArrayStr(data.female)}
-male:${getYamlArrayStr(data.male)}
-mixed:${getYamlArrayStr(data.mixed)}
-location:${getYamlArrayStr(data.location)}
-other:${getYamlArrayStr(data.other)}
+categories:${this.util.getYamlArrayStr(data.categories)}
+keywords:${this.util.getYamlArrayStr(data.keywords)}
+female:${this.util.getYamlArrayStr(data.female)}
+male:${this.util.getYamlArrayStr(data.male)}
+mixed:${this.util.getYamlArrayStr(data.mixed)}
+location:${this.util.getYamlArrayStr(data.location)}
+other:${this.util.getYamlArrayStr(data.other)}
 english: "${data.english}"
 japanese: "${data.japanese}"
 url: "${data.url}"
-artist:${getYamlArrayStr(data.artist)}
-group:${getYamlArrayStr(data.group)}
-parody:${getYamlArrayStr(data.parody)}
-character:${getYamlArrayStr(data.character)}
-language:${getYamlArrayStr(data.language)}
+artist:${this.util.getYamlArrayStr(data.artist)}
+group:${this.util.getYamlArrayStr(data.group)}
+parody:${this.util.getYamlArrayStr(data.parody)}
+character:${this.util.getYamlArrayStr(data.character)}
+language:${this.util.getYamlArrayStr(data.language)}
 pagecount: ${data.pagecount}
 cover: "${await data.coverPromise}"
-uploader:${getYamlArrayStr(data.uploader)}
+uploader:${this.util.getYamlArrayStr(data.uploader)}
 parent: "${data.parent}"
 visible: "${data.visible}"
 filesize: "${data.filesize}"
@@ -199,7 +174,7 @@ favorited: ${data.favorited}
 rating: ${data.rating}
 uploaded: ${data.uploaded}
 ctime: ${data.ctime}
-mtime: ${data.mtime}${getUnindexedDataFrontMatterPartStrBlock(data.unindexedData)}
+mtime: ${data.mtime}${this.util.getUnindexedDataFrontMatterPartStrBlock(data.unindexedData)}
 ---
 
 # ${data.title}
@@ -208,8 +183,8 @@ mtime: ${data.mtime}${getUnindexedDataFrontMatterPartStrBlock(data.unindexedData
 
 | | |
 | --- | --- |
-| title_en | \`${escapePipe(data.english)}\` |
-| title_jp | \`${escapePipe(data.japanese)}\` |
+| title_en | \`${this.util.escapePipe(data.english)}\` |
+| title_jp | \`${this.util.escapePipe(data.japanese)}\` |
 | url | ${data.url} |
 | parody | ${data.parody.join(", ")} |
 | character | ${data.character.join(", ")} |
@@ -230,46 +205,102 @@ mtime: ${data.mtime}${getUnindexedDataFrontMatterPartStrBlock(data.unindexedData
 | visible | ${data.visible} |
 | filesize | ${data.filesize} |
 | favorited | ${data.favorited} |
-| rating | ${data.rating} |${getUnindexedDataTablePartStrBlock(data.unindexedData)}
+| rating | ${data.rating} |${this.util.getUnindexedDataTablePartStrBlock(data.unindexedData)}
 `;
+    }
   }
 
-  // Helpers
-  function sanitizeTitle(str) {
-    return (str + " 【exhentai】")
-      .replaceAll("[", "【")
-      .replaceAll("]", "】")
-      .replaceAll(/[?:]/g, "_")
-      .replaceAll(/\s{2,}/g, " ");
+  // utils  
+
+  class Util {
+    startWebclipperWithDelay(timeout, message, getGalleryData, getOBMDNoteFileContent) {
+      setTimeout(async () => {
+        if (confirm(message)) {
+          const galleryData = getGalleryData();
+          const obsidianURI = this.getObsidianURI(
+            galleryData.title, await getOBMDNoteFileContent(galleryData)
+          );
+          window.location.href = obsidianURI;
+        }
+      }, timeout);
+    }
+
+    // Build Obsidian URI
+    getObsidianURI(theOBMDNotefileBaseName, theOBMDNoteFileContent) {
+      const params = [
+        ["file", `acg/galleries/${theOBMDNotefileBaseName}`],
+        ["content", theOBMDNoteFileContent],
+        ["append", "1"]
+      ].map(([k, v]) => `${k}=${encodeURIComponent(v)}`).join("&");
+
+      return `obsidian://new?${params};`;
+    }
+
+    getUnindexedDataFrontMatterPartStrBlock(unindexedData) {
+      let unindexedDataFrontMatterPartStrBlock = '';
+      Object.entries(unindexedData).forEach(([key, value]) => {
+        if (Array.isArray(value)) {
+          unindexedDataFrontMatterPartStrBlock += `\n${key}:${this.getYamlArrayStr(value)}`;
+        } else {
+          unindexedDataFrontMatterPartStrBlock += `\n${key}: "${value}"`;
+        }
+      });
+      return unindexedDataFrontMatterPartStrBlock;
+    }
+
+    getUnindexedDataTablePartStrBlock(unindexedData) {
+      let unindexedDataTablePartStrBlock = '';
+      Object.entries(unindexedData).forEach(([key, value]) => {
+        if (Array.isArray(value)) {
+          unindexedDataTablePartStrBlock += `\n| ${key} | ${value.join(", ")} |`;
+        } else {
+          unindexedDataTablePartStrBlock += `\n| ${key} | ${value} |`;
+        }
+      });
+      return unindexedDataTablePartStrBlock;
+    }
+
+    escapePipe(str) {
+      return str.replace(/\|/g, "\\|");
+    }
+
+    sanitizeTitle(titleStr, addtionalSuffix) {
+      return (titleStr + addtionalSuffix)
+        .replaceAll("[", "【")
+        .replaceAll("]", "】")
+        .replaceAll(/[?:]/g, "_")
+        .replaceAll(/\s{2,}/g, " ");
+    }
+
+    getTitleStr(titleEl) {
+      if (!titleEl) return "";
+      return titleEl.innerText.replace(/\s{2,}/g, " ");
+    }
+
+    getTagNameStr(str) {
+      return str.trim()
+        .replace(/\s+/g, "-")
+        .replace("-|-", "-or-");
+    }
+
+    getLocalISOStringWithTimezone() {
+      const date = new Date();
+      const pad = n => String(n).padStart(2, "0");
+
+      const offset = -date.getTimezoneOffset(); // actual UTC offset in minutes
+      const sign = offset >= 0 ? "+" : "-";
+      const hours = pad(Math.floor(Math.abs(offset) / 60));
+      const minutes = pad(Math.abs(offset) % 60);
+
+      return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T` +
+        `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}` +
+        `${sign}${hours}:${minutes}`;
+    }
+
+    getYamlArrayStr(arr) {
+      return arr.map(i => `\n  - "${i}"`).join("");
+    }
   }
 
-  function getTitleStr(titleEl) {
-    if (!titleEl) return "";
-    return titleEl.innerText.replace(/\s{2,}/g, " ");
-  }
-
-  function getTagNameStr(str) {
-    return str.trim()
-      .replace(/\s+/g, "-")
-      .replace("-|-", "-or-");
-  }
-
-  function getLocalISOStringWithTimezone() {
-    const date = new Date();
-    const pad = n => String(n).padStart(2, "0");
-
-    const offset = -date.getTimezoneOffset(); // actual UTC offset in minutes
-    const sign = offset >= 0 ? "+" : "-";
-    const hours = pad(Math.floor(Math.abs(offset) / 60));
-    const minutes = pad(Math.abs(offset) % 60);
-
-    return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T` +
-      `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}` +
-      `${sign}${hours}:${minutes}`;
-  }
-
-  function getYamlArrayStr(arr) {
-    return arr.map(i => `\n  - "${i}"`).join("");
-  }
-
+  Main.main();
 })();
